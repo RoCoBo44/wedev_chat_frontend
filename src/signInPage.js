8a8126707c06163c0ff839d3c3374d3e39de3578
RoCobo <39014389+RoCobo@users.noreply.github.com>
2020-06-17 13:47:59 -0300

    connected signinPage with backend

--- src/signInPage.js
+++ src/signInPage.js
@@ -4,11 +4,41 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import {useForm} from "react-hook-form"
 import { Link,Route} from 'react-router-dom';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { graphql } from 'react-apollo'
+import { useState } from 'react'
+
+const SIGN_IN = gql`
+    mutation login ($data: SigninInput!){
+        signin(data: $data){
+        user{
+            username
+            firstName
+            lastName
+        }
+        jwt
+        authError
+        }
+    }
+`;
 
 function SignInPage (props){
     const {register, handleSubmit, errors }  = useForm();
+    const [signIn_mutation, { data }] = useMutation(SIGN_IN);
+    const [mutationError, setMutationError] = useState(false);
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = async (values) => {
+        const response = await signIn_mutation({ variables: { data: {"username": values.username, "password" : values.password} } });
+        console.log(response);
+        if (response.data.signin.authError == null){
+            let url = "http://localhost:3000/home"
+            window.location.href = url;
+        }else{
+            console.log("Login error");
+            setMutationError(true);
+        }
+    }
 
     return (
     <Container className='form-wrapper'>
@@ -21,6 +51,7 @@ function SignInPage (props){
                     placeholder="Username"
                     name= "username"
                     ref = {register}
+                    isInvalid ={mutationError}
                 />
             </Form.Group>
 
@@ -30,8 +61,14 @@ function SignInPage (props){
                     placeholder="Password"
                     name= "password"
                     ref = {register}
+                    isInvalid ={mutationError}
                 />
             </Form.Group>
+
+            {mutationError &&   <div className="alert alert-dark" role="alert">
+                                    Upsi dupsi, user or password is incorrect 
+                                </div>
+            }
 
             <Button type="submit">Sign In</Button>
 
@@ -48,4 +85,4 @@ function SignInPage (props){
     );
 }
 
-export default SignInPage;
+export default graphql(SIGN_IN)(SignInPage);
